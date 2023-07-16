6c2b413221b4837299e128e7776650504d6319b9
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2023-07-16 01:58:01 +0100

    first commit

---
+++ client/src/scenes/homePage/index.js
@@ -0,0 +1,43 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+
+const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) =>state.user);
+
+
+  const dispatch = useDispatch();
+ 
+  const token = useSelector((state) => state.token);
+  const user = useSelector((state) => state.user);
+console.log(user);
+  const getUser = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${_id}`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+   console.log(data);
+  };
+  return (
+    <Box>
+   
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+          
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
+      </Box>
+  
+  );
+};
+
+export default HomePage;
---
+++ client/src/scenes/widgets/MyPostWidget.js
@@ -0,0 +1,117 @@
+import {
+    EditOutlined,
+    DeleteOutlined,
+    AttachFileOutlined,
+    GifBoxOutlined,
+    ImageOutlined,
+    MicOutlined,
+    MoreHorizOutlined,
+  } from "@mui/icons-material";
+  import {
+    Box,
+    Divider,
+    Typography,
+    InputBase,
+    useTheme,
+    Button,
+    IconButton,
+    useMediaQuery,
+  } from "@mui/material";
+  import FlexBetween from "components/FlexBetween";
+  import Dropzone from "react-dropzone";
+  import UserImage from "components/UserImage";
+  import WidgetWrapper from "components/WidgetWrapper";
+  import { useState } from "react";
+  import { useDispatch, useSelector } from "react-redux";
+  import { setPosts } from "state";
+  
+  const MyPostWidget = ({ picturePath }) => {
+    const dispatch = useDispatch();
+    
+    const [post, setPost] = useState("");
+    const { palette } = useTheme();
+    const { _id } = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.neutral.medium;
+  
+    const handlePost = async () => {
+      const formData = new FormData();
+      formData.append("userId", _id);
+      formData.append("description", post);
+     
+  
+      const response = await fetch(`http://localhost:3001/posts`, {
+        method: "POST",
+        headers: { Authorization: `Bearer ${token}` },
+        body: formData,
+      });
+      const posts = await response.json();
+      dispatch(setPosts({ posts }));
+      
+      setPost("");
+    };
+  
+    return (
+      <WidgetWrapper>
+        <FlexBetween gap="1.5rem">
+          {/* <UserImage image={picturePath} /> */}
+          <InputBase
+            placeholder="What's on your mind..."
+            onChange={(e) => setPost(e.target.value)}
+            value={post}
+            sx={{
+              width: "100%",
+              backgroundColor: palette.neutral.light,
+              borderRadius: "2rem",
+              padding: "1rem 2rem",
+            }}
+          />
+        </FlexBetween>
+       
+  
+        <Divider sx={{ margin: "1.25rem 0" }} />
+  
+        <FlexBetween>
+         
+          {isNonMobileScreens ? (
+            <>
+              <FlexBetween gap="0.25rem">
+                <GifBoxOutlined sx={{ color: mediumMain }} />
+                <Typography color={mediumMain}>Clip</Typography>
+              </FlexBetween>
+  
+              <FlexBetween gap="0.25rem">
+                <AttachFileOutlined sx={{ color: mediumMain }} />
+                <Typography color={mediumMain}>Attachment</Typography>
+              </FlexBetween>
+  
+              <FlexBetween gap="0.25rem">
+                <MicOutlined sx={{ color: mediumMain }} />
+                <Typography color={mediumMain}>Audio</Typography>
+              </FlexBetween>
+            </>
+          ) : (
+            <FlexBetween gap="0.25rem">
+              <MoreHorizOutlined sx={{ color: mediumMain }} />
+            </FlexBetween>
+          )}
+  
+          <Button
+            disabled={!post}
+            onClick={handlePost}
+            sx={{
+              color: palette.background.alt,
+              backgroundColor: palette.primary.main,
+              borderRadius: "3rem",
+            }}
+          >
+            POST
+          </Button>
+        </FlexBetween>
+      </WidgetWrapper>
+    );
+  };
+  
+  export default MyPostWidget;
